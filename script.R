58143c5185f9812a75951b372f21b2c24da0ebc2
Nicholas Michaud <michaud@iastate.edu>
2015-02-23 12:50:08 -0600

    updated comments slightly

--- script.R
+++ script.R
@@ -17,14 +17,14 @@ dates = c("2014-07-01","2015-06-30")
 # Extract data for desired location
 
 # First we take our data table and select only two columns from it: 
-# 1. The location column corresponding to the location that the user chose from the drop down menu in ui.R (called input$location)
+# 1. The location column corresponding to the location that we chose above
 # 2. The Date column, which we will need to plot our data
 
 selected_data = select_(.data = google_data, 
                          Location = location, 
                          "Date") 
 
-# Next, we filter the data so that only dates that fall within the user's selected date range (input$dateRange) are returned.
+# Next, we filter the data so that only dates that fall within the user's selected date range (dates[1] and dates[2]) are returned.
 filtered_data = filter(selected_data, 
                        Date >=dates[1] & Date <= dates[2] )
 
@@ -33,11 +33,11 @@ filtered_data = filter(selected_data,
 # To use the ggplot function (part of the ggplot2 library), specify:
 # the x variable (here, Date), 
 # the y variable( here, Location)
-# the data that you are using to make the plot.  Our data is provided by the selectedData() function above
-# the plot type you'd like to make. We use geom="line" to make a line connecting the data points in our plot
+# the data that you are using to make the plot.  Our data is the filtered_data we created above
 
 ggplot(data = filtered_data, 
        aes(x = Date, 
            y = Location)) + 
-  geom_line() + 
-  labs(y ='', title = location)
+  geom_line() + # add geom_line() to make a line connecting the data points in our plot
+  labs(y ='', title = location) # set labels for the axes and the title of the plot
+
